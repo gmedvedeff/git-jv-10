3d934515cf8d490b3baf16bba751ab908a7ba19e
= <lmorrassn@gmail.com>
2021-07-07 14:58:53 -0300

    Errores incorporados

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 /* Navigation */
     menuBtn = document.querySelector('nav .button')
-    menuLst = document.querySelector('nav .menu')
+    menuLst = document.querySelector('nav.menu')
 
     menuBtn.onclick = () => menuLst.classList.toggle('active') ? menuBtn.innerHTML = '&times;' : menuBtn.innerHTML = '&equiv;'
